aac58b5f3e612e337df9f89374e6cef409a8c436
OpLpEpG <ooorivas@gmail.com>
2019-03-14 17:15:38 +0500

    day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootloader';
+  inpText = 'lorem100'; 
+  metatext = 'metatext';
+  help_file = 'свойства файла';
+  adr = 3;
+  /**
+   * fileChangeEvent
+      event: any   
+  */
+  public fileChangeEvent(fileInput: any){
+    if (fileInput.target.files && fileInput.target.files[0]) {
+
+      let f: File = fileInput.target.files[0];
+      this.help_file = `время файла: ${new Date(f.lastModified).toLocaleString()}
+                        размер файла: ${f.size}`;
+
+      let reader = new FileReader();
+
+      reader.onload = ( e:any) => this.metatext = e.target.result;
+      
+      reader.readAsDataURL(f);
+  }
+}
 }
